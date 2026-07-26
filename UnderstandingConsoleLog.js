585b0cf36e8e551f8df243fe812b22ca43defb4f
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-07-26 16:25:08 +0530

    Document console.log() functionality and behavior
    Added explanations and comments about console.log() behavior.

---
+++ UnderstandingConsoleLog.js
@@ -0,0 +1,31 @@
+// Debugging functions
+
+/**
+ * Why we are doing return 
+ * Can we not use console.log() ?
+ **/ 
+
+
+// WHAT IS console.log()?
+//---> prints value on the console
+// console.log is also a function
+
+
+// console---> object
+// log--> key
+
+/** console is an object
+ * console ={
+ *          log:function(){}
+ *         // ...........
+ * }
+ */
+
+// if console.log is a function , does it return any value?
+// it returns undefined.....
+
+x = console.log("dsa"); // dsa is input value given to the function and
+// this function prints the given input value on the screen and 
+//and returns undefined no matter what the input was.
+
+console.log(x);      // undefined
