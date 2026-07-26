e8480b9f84b6e55ab76d459e6aca3ee243e1118d
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-07-26 16:45:28 +0530

    Enhance UnderstandingConsoleLog.js with return value info
    Added explanations about function return values and examples.

--- UnderstandingConsoleLog.js
+++ UnderstandingConsoleLog.js
@@ -29,3 +29,29 @@ x = console.log("dsa"); // dsa is input value given to the function and
 //and returns undefined no matter what the input was.
 
 console.log(x);      // undefined
+
+
+
+// Function will always return something , it is upto the user if we wanna store it or do something with it or leave it
+
+// We use return in function so that we can store that value and use it somewhere else.
+
+// What if i dont return anything in the function---> In js, if you don't manually return something, it automatically returns undefined.
+
+function greet(name){
+    console.log("Hello", name, "WELCOME TO JAVASCRIPT");
+    
+}
+
+let result = greet("Dharmendra");
+console.log(result);
+// greet returns undefined
+
+function greeting(name){
+    console.log("Hello", name, "WELCOME TO JAVASCRIPT");
+    return "Welcomed the user";
+}
+
+let res = greeting("Narendra");
+console.log(res);
+// here the function is returning "Welcomed the user"
