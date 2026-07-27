d154a8b2d9d3a213d826e3ff5222a5a92c7dea93
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-07-27 13:39:31 +0530

    Add examples of variable scoping with var
    Demonstrate variable scope in JavaScript using var.

---
+++ ScopesINJavaScript/letVarConst.js
@@ -0,0 +1,34 @@
+
+{
+    var p = 10;       // here p has global scope
+    console.log(p);
+}
+console.log(p);
+
+function fun(){
+    console.log(p1)    // Why we get undefined ---> LEXICAL SCOPING
+    var p1 = 10;       // here p1 has function scope
+    console.log(p1);
+}
+
+fun();
+// undefined       -----> LEXICAL SCOPING
+// 10
+{
+    var v = 8; // becomes global due to var
+}
+console.log(v);
+//*** var --> if written outside a function , gives the variable global scope***
+
+if(true){
+    var z = 10;
+}
+
+if(false){
+    var d = 11;
+}
+
+console.log(z);     // 10
+console.log(d);     // undefined --> Lexical scoping
+
+// read more about var on mdn 
