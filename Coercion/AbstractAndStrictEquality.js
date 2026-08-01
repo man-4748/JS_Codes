39c4351098b884e4b08b5e5ac10400015facbc44
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-01 11:33:11 +0530

    Update AbstractAndStrictEquality.js

--- Coercion/AbstractAndStrictEquality.js
+++ Coercion/AbstractAndStrictEquality.js
@@ -25,11 +25,35 @@ console.log(null == false);
  * null == 0 -----> no rules in docs for this ---> return false
  */
 
+console.log("NaN" === NaN); // types are different  ----> return false
 
+console.log(NaN === NaN) // false
 
+console.log(0 === -0) // false
 
+// x == y 
+// y is an object -->  x == ToPrimitive(y)
+let obj ={
+    x:10,
+    valueOf(){
+        return 100;
+    }
+}
+//ToPrimitive(obj) -->OrdinaryToPrimitive(obj,hint) --> valueOf --> return 100 (non object)
+console.log(99 == obj);   // false
+console.log(100 == obj);  // true
 
+// Strict equality
 
+let obj1 = {x:10};
+let obj2 = {x:10};
+let obj3 = {y:10};
+console.log(obj1 === obj2);     // objects are present at different memory locations
+console.log(obj1 === obj3);
+console.log(obj2 === obj3);
+console.log(obj1 === obj1);     // true
+
+console.log({a:10}==={a:10});  // these both objects will be created at different locations in the memory
 
 
 
