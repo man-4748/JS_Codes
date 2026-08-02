dbb0e8ee8caf128def030b8b1d5e152753702b72
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-02 10:10:23 +0530

    Create BoxInJs.js with coercion examples
    Add examples of coercion and boxing in JavaScript.

---
+++ Coercion/BoxingInJs.js
@@ -0,0 +1,9 @@
+//console.log(1.toString());  invalid
+
+console.log("avb".toString());
+// boxing
+
+console.log((1).toString());
+// (1) ---> got converted to object {1}
+
+// Boxing ---> JS primitive types gets converted to non primitives if you use boxing
