987274bd8f23fd8bc99ecc1028a3b4466e2e0105
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-05 21:55:40 +0530

    Update letVarConst.js

--- ScopesINJavaScript/letVarConst.js
+++ ScopesINJavaScript/letVarConst.js
@@ -32,3 +32,29 @@ console.log(z);     // 10
 console.log(d);     // undefined --> Lexical scoping
 
 // read more about var on mdn 
+
+
+{
+    let x = 2;
+    console.log(x);
+
+}
+// console.log(x); Error: x is not defined 
+// Let ---> Block scope   --> accessible only inside if/else or raw block or function block;
+
+function Letf(){
+    //console.log(x); Cannot access 'x' before initialization
+    let x = 1;
+    console.log(x);
+}
+
+function Varf(){
+    console.log(v);  //undefined
+    var v = 1;
+    console.log(v);
+}
+Letf();   // point to be noted if you don't call this function , rest of code gets executed without any error ---> somewhat js does read code twice
+Varf();
+// console.log(x);
+// console.log(v);
+// both will give error
