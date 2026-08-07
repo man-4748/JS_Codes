3f2256a90a91e35f81d973f9cfb6d4b615dfee92
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-07 15:58:23 +0530

    Implement variable scope example in useCase.js
    Added a new use case demonstrating variable scope in JavaScript.

---
+++ ScopesINJavaScript/useCase.js
@@ -0,0 +1,28 @@
+var x = 10;
+/**
+ * 
+ * 
+ * 
+ * 
+ * lots of code
+ */
+console.log(x);
+/*
+ * 
+ * 
+ * 
+ * 
+ * lots of code
+ */
+// new code
+//var x = 20; // redeclared  there can be problem in the code due to this
+
+
+// you can declare the new code in a block scope ---> new code will be executed without any hassle
+
+{
+    // new code
+    var x = 20;         
+    // end of new code 
+
+}
