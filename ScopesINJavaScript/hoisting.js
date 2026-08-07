84bebde4bc05b6c3dcc303498188418d0284016e
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-07 14:40:54 +0530

    Implement function to illustrate hoisting concept
    Added a function 'gun' to demonstrate hoisting in JavaScript.

---
+++ ScopesINJavaScript/hoisting.js
@@ -0,0 +1,11 @@
+gun();
+function gun(){
+    console.log("Hello World");
+}
+
+/**
+ * Declaring a function before calling it
+ * Hoisting is nothing but a direct consequence of Lexical scoping
+ * Nothing such as moving functions to top of the code happens while parsing, its incorrect.
+ */
+
