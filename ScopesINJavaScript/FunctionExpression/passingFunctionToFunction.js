e792f157e9a05d4d2d1b912f03631ba90ecdec22
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-09 09:53:09 +0530

    Implement function declaration and passing functions
    Added examples of function declaration, function expression, and passing functions as arguments.

---
+++ ScopesINJavaScript/FunctionExpression/passingFunctionToFunction.js
@@ -0,0 +1,26 @@
+// Function declaration
+function greet() {
+    console.log("Hello");
+}
+
+// Function expression      
+// Function expression = a function that is created as part of an expression and can be treated like a value.
+const greet = function () {
+    console.log("Hello");     // The function is created as a value and assigned to greet. 
+};
+
+// Passing function as an argument to another function 
+function  fun(){
+    console.log("fun is called");
+}// this is a function definition ---> whenever you start a piece of code with Keyword "function".
+
+function gun(fn){
+    console.log("Inside gun");
+    fn();
+    console.log("Leaving gun");
+}
+gun(function (){// this is a function expression ---> whenever we don't start writing the code with Keyword function.
+    console.log("New function passed.")
+});
+
+gun(fun);
