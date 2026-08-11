b8bc7ac6c6cd015ab895bf05ba9f44e0e09ce954
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-11 11:14:45 +0530

    Enhance callbackDemo.js with array map examples
    Added examples of using callbacks with array methods.

--- ClosuresAndCallbacks/callbackDemo.js
+++ ClosuresAndCallbacks/callbackDemo.js
@@ -12,3 +12,21 @@ function fun(x,fn){
 fun(10, function log(){
     console.log("custom logger"); 
 })
+
+
+
+// applications of callback:
+
+let arr = [1,2,4,3,5,6];
+let x =arr.map(function process(v,i){
+    // v---> value , i---> index
+    // map : for all the elements in the given array, it passes as an argument to the callback
+    console.log(v,i);
+})
+
+console.log(x);// ---> undefined array because we are not returning anything inside callback function
+let y = arr.map(function parse(v,i){
+    console.log(i,v*v);
+    return v*v;
+})
+console.log(y);
