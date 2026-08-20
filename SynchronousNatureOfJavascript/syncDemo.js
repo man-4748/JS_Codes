f77dec35583f5c46a3e8b197f31c5ff48b921d8e
man-4748 <156610048+man-4748@users.noreply.github.com>
2026-08-20 12:58:36 +0530

    Update syncDemo.js

--- SynchronousNatureOfJavascript/syncDemo.js
+++ SynchronousNatureOfJavascript/syncDemo.js
@@ -6,3 +6,6 @@ for(let i=0;i<1000000000;i++){
 console.log("Task done",count);
 console.log("End");
 // Line 6 is executed after some time bec js is sync in nature. Line 6 only gets executed after loop is finished
+
+//Single-Threaded & Synchronous: JavaScript executes code on a single thread with a single Call Stack. 
+//It processes instructions one line at a time from top to bottom.
